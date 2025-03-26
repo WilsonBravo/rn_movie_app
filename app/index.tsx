76b3d70c739e184d7a20ce3248832632175a054b
Wilson Bravo <bravoc.wilson@gmail.com>
2025-03-26 16:53:18 -0500

    add nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            className="flex-1 items-center justify-center"
+        >
+            <Text className="text-5xl text-[#123123] font-bold">Welcome!</Text>
+        </View>
+    );
+}
